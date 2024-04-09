ff60381bac5c36ccfad905768ef1770a3fa30650
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2024-04-08 21:56:53 -0300

    chore: create module, controller and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -33,7 +33,7 @@
 //   }
 // }
 
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
@@ -44,5 +44,10 @@ export class UserController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findByEmail(@Body() email: string) {
+    return this.userService.findByEmail(email);
   }
 }
