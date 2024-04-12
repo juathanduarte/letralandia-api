a8b7390720e5bd4d41e065b0de7df76caa4a4bad
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2024-04-11 21:13:46 -0300

    feath: create guards and modules

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateUser(email: string, password: string) {
+    return { email };
+  }
+  login() {
+    return 'This action logs a user in';
+  }
+}
